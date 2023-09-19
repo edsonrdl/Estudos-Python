ce2927e32bb0b8ebda91295f4e9b2f47478c34ef
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-09-19 00:54:50 -0300

    feat:Criado exemplo de operadores aritméticos

--- 1. Sintaxe Básica/1.2. Operadores/1.2.1. Operadores Aritméticos/operadoresAritmeticosEx01.py
+++ 1. Sintaxe Básica/1.2. Operadores/1.2.1. Operadores Aritméticos/operadoresAritmeticosEx01.py
@@ -1,27 +1,41 @@
 #Adição (+):
 
 #Ex :01
-a = 5
-b = 3
-resultado = a + b
-print(resultado) # Resultado: 8
+num1Ex01 = 5
+num2Ex01 = 3
+resultadoEx01 = num1Ex01 + num2Ex01
+print("Resultado da adição =",resultadoEx01) # Resultado: 8
 
 
 #Subtração (-):
 
-#Ex : 01
-a = 10
-b = 4
-resultado = a - b
-print(resultado)  # Resultado: 6
+#Ex : 02
+num1Ex02 = 10
+num2Ex02 = 4
+resultadoEx02 = num1Ex02 - num2Ex02
+print("Resultado da subtração =",resultadoEx02)  # Resultado: 6
 
 #Multiplicação (*):
 
-#Ex : 01
-a = 7
-b = 3
-resultado = a * b
-print(resultado)  # Resultado: 21
+#Ex : 03
+num1Ex03 = 7
+num2Ex03 = 3
+resultadoEx03 = num1Ex03 * num2Ex03
+print("Resultado da multiplicação =",resultadoEx03)  # Resultado: 21
 
 #Divisão (/):
- #Ex:
+
+#Ex: 04 
+num1Ex04 = 10
+num2Ex04 = 2
+resultadoEx04 = num1Ex04 / num2Ex04
+print("Resultado da divisão =",resultadoEx04)  # Resultado: 5.0
+
+
+#Exponenciação (**):
+
+#Ex : 05
+num1Ex05=5
+num2Ex05=2
+resultadoEx05=num1Ex05**num2Ex05
+print("Resultado da exponencialção é =",resultadoEx05)
